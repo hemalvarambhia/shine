f29a3ac8e4696e87e201d2335772832187317821
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-11-10 16:50:48 +0000

    The user can log out of the application.

---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    email 'user2302@email.com'
+    password 'secret'
+  end
+end
--- spec/views/dashboard/index_spec.rb
+++ spec/views/dashboard/index_spec.rb
@@ -2,5 +2,12 @@ require 'rails_helper'
 describe 'dashboard/index' do
   include Devise::Test::ControllerHelpers
   
-  it 'allows the user to log out'
+  it 'allows the user to log out' do
+    user = create(:user)
+    sign_in user
+    
+    render
+    
+    expect(rendered).to match /Log Out/
+  end
 end
