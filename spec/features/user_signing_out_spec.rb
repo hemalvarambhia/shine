3fddf8afc97cd4f27addc9a08cd75444bfeb1833
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-11-11 09:08:38 +0000

    A user can log out of Shine.

--- spec/features/user_signing_out_spec.rb
+++ spec/features/user_signing_out_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.feature 'User session management', type: :feature do
-  scenario 'User signs out'
+  before :each do
+    User.create(email: 'test_user@example.com', password: 'secret')
+  end
+  
+  scenario 'User signs out' do
+    visit '/'
+    fill_in 'Email', with: 'test_user@example.com'
+    fill_in 'Password', with: 'secret'
+    click_button 'Log in'
+
+    click_link 'Log Out'
+
+    expect(page).to have_button 'Log in'
+  end
 end
