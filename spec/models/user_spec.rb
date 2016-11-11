8b1ffb860ca4020f1af36f306db36bcb40242406
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-11-11 10:56:58 +0000

    Test list - a user account with a password more than 128 characters long is invalid.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,8 @@ describe 'User account' do
   context 'with a password less than 10 characters long' do
     it 'is invalid'
   end
+
+  context 'with a password more than 128 characters long' do
+    it 'is invalid'
+  end
 end
