5568fd13278f3b3ccd3fae9d07638c09a878d281
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-11-15 13:57:34 +0000

    Test list - the user should be able to search for customers by their e-mail address.

--- spec/features/searching_for_customers_spec.rb
+++ spec/features/searching_for_customers_spec.rb
@@ -4,4 +4,6 @@ describe 'Searching for customers' do
   it 'finds customers by matching first name'
 
   it 'finds customers by matching last name'
+
+  it 'finds customers by their e-mail address'
 end
