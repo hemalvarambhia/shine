d7c3c554a60129706713cb36a9653841c2d55582
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-11-26 13:25:05 +0000

    We now retrieve the customers matching the search term asynchronously (ajax).

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,34 +1,20 @@
 app = angular.module("customers", [])
 
-var CustomerSearchController = function($scope) {
-    $scope.customers = []
+var CustomerSearchController = function($scope, $http) {
+    $scope.customers = [];
     $scope.search = function(searchTerm) {
-	$scope.customers = [
-	    {
-		"first_name":"Schuyler",
-		"last_name":"Cremin",
-		"email": "giles@macgyver.net",
-		"username":"jillian0",
-		"created_at":"2015-03-04"
-	    },
-	    {
-		"first_name":"Derick",
-		"last_name":"Ebert",
-		"email": "lupe1@rennerfisher.org",
-		"username":"ubaldo_kaulke1",
-		"created_at":"2015-03-04"
-	    },
-	    {
-		"first_name":"Derick",
-		"last_name":"Johnsons",
-		"email": "dj@somewhere.org",
-		"username":"djj",
-		"created_at":"2015-03-04"
-	    }
-	]
+	$http.get("/customers.json",
+		  { "params": {"keywords": searchTerm } }
+		 ).then(function(response) {
+		     $scope.customers = response.data;
+		 }, function(response) {
+		     alert("There was a problem: " + response.status);
+		 }
+		       );
     }
-};
+}
 
-app.controller("CustomerSearchController",
-	       [ "$scope", CustomerSearchController ]
-	      );
+app.controller(
+    "CustomerSearchController",
+    [ "$scope", "$http", CustomerSearchController ]
+);
