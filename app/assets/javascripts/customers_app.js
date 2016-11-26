b75ef580556dc503a6b28ca817ac4e781bd85454
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-11-26 14:02:33 +0000

    We have built the typeahead search feature now.

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -4,6 +4,9 @@ var CustomerSearchController = function($scope, $http) {
     var page = 0;
     $scope.customers = [];
     $scope.search = function(searchTerm) {
+	if (searchTerm.length < 3) {
+	    return;
+	}
 	$http.get("/customers.json",
 		  { "params": {"keywords": searchTerm, "page": page } }
 		 ).then(function(response) {
