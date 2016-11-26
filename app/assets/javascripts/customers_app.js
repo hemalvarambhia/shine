6824d4fc53532dbb59abed370e9a63845b191f52
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-11-26 13:13:45 +0000

    We have set up the Angular controller to return canned results as a JSON doc.

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,8 +1,31 @@
 app = angular.module("customers", [])
 
 var CustomerSearchController = function($scope) {
+    $scope.customers = []
     $scope.search = function(searchTerm) {
-	$scope.searchedFor = searchTerm;
+	$scope.customers = [
+	    {
+		"first_name":"Schuyler",
+		"last_name":"Cremin",
+		"email": "giles@macgyver.net",
+		"username":"jillian0",
+		"created_at":"2015-03-04"
+	    },
+	    {
+		"first_name":"Derick",
+		"last_name":"Ebert",
+		"email": "lupe1@rennerfisher.org",
+		"username":"ubaldo_kaulke1",
+		"created_at":"2015-03-04"
+	    },
+	    {
+		"first_name":"Derick",
+		"last_name":"Johnsons",
+		"email": "dj@somewhere.org",
+		"username":"djj",
+		"created_at":"2015-03-04"
+	    }
+	]
     }
 };
 
