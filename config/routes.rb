d07170548670d16da9662b81a062b49967dbc70a
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-12-17 06:38:27 +0000

    Set up the API that returns a customer's details given an ID.

--- config/routes.rb
+++ config/routes.rb
@@ -59,5 +59,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   root 'dashboard#index'
-  resources :customers, only: [ :index ]
+  resources :customers, only: [ :index, :show ]
 end
